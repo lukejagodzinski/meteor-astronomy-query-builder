31641705af79598eebf3626ab602986b33ee6a54
Lukasz Jagodzinski <luke.jagodzinski@gmail.com>
2015-05-25 23:38:05 +0200

    Update astronomy version

--- package.js
+++ package.js
@@ -8,7 +8,7 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
-  api.use('jagi:astronomy@0.9.0');
+  api.use('jagi:astronomy@0.9.1');
   api.use('underscore');
 
   api.imply('jagi:astronomy');
